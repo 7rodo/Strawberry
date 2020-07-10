80a0dc301b2d2ad7b840e995e1b5c587009a8615
7rodo <7rodovje7878@gmail.com>
2020-07-10 13:08:29 +0100

    Update thermal-reactor.js

--- scripts/thermal-reactor.js
+++ scripts/thermal-reactor.js
@@ -8,14 +8,13 @@ const thermalReactor = extendContent(ThermalGenerator, "thermal-reactor", {
         }
     },
     draw(tile){
-  entity = tile.ent();
   Draw.rect(this.bottomRegion, tile.drawx(), tile.drawy());
   for(var i = 0; i < 4; i++){
     r = this.size * Vars.tilesize - 3 + Mathf.absin(Time.time(), 2 + i * 1, 5 - i * 0.5);
     Draw.color(Color.valueOf("ffd06b"), Color.valueOf("ff361b"), i / 4);
-    Draw.alpha((0.3 + Mathf.absin(Time.time(), 2 + i * 2, 0.3 + i * 0.05)) * entity.warmup);
+    Draw.alpha((0.3 + Mathf.absin(Time.time(), 2 + i * 2, 0.3 + i * 0.05)));
     Draw.blend(Blending.additive);
-    Draw.rect(this.plasmaRegions[i], tile.drawx(), tile.drawy(), r, r, Time.time() * (12 + i * 6) * entity.warmup);
+    Draw.rect(this.plasmaRegions[i], tile.drawx(), tile.drawy(), r, r, Time.time() * (12 + i * 6));
     Draw.blend();
   }
   Draw.color();
