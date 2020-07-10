e79c03d892d02163a0f7a678927071926abc4b58
7rodo <7rodovje7878@gmail.com>
2020-07-10 12:22:42 +0100

    Fixed a mistake

--- scripts/thermal-reactor.js
+++ scripts/thermal-reactor.js
@@ -12,7 +12,7 @@ const thermalReactor = extendContent(ThermalGenerator, "thermal-reactor", {
   Draw.rect(this.bottomRegion, tile.drawx(), tile.drawy());
   for(var i = 0; i < 4; i++){
     r = this.size * Vars.tilesize - 3 + Mathf.absin(Time.time(), 2 + i * 1, 5 - i * 0.5);
-    Draw.color(Color.valueOf("hex"), Color.valueOf("hex"), i / 4);
+    Draw.color(Color.valueOf("ffd06b"), Color.valueOf("ff361b"), i / 4);
     Draw.alpha((0.3 + Mathf.absin(Time.time(), 2 + i * 2, 0.3 + i * 0.05)) * entity.warmup);
     Draw.blend(Blending.additive);
     Draw.rect(this.plasmaRegions[i], tile.drawx(), tile.drawy(), r, r, Time.time() * (12 + i * 6) * entity.warmup);
