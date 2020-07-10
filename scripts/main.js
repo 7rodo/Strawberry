5406a95c4b460ab325113e97cfee11604f17d536
7rodo <7rodovje7878@gmail.com>
2020-07-10 12:06:51 +0100

    Added the mod
    j

---
+++ scripts/main.js
@@ -0,0 +1,2 @@
+require('purifier');
+require('thermal-reactor');
---
+++ scripts/thermal-reactor.js
@@ -0,0 +1,25 @@
+const thermalReactor = extendContent(ThermalGenerator, "thermal-reactor", {
+    load(){
+        this.super$load();
+        this.plasmaframes = [];
+        this.bottomRegion = Core.atlas.find(this.name + "-bottom");
+        for(i = 0; i < 4; i++){
+            this.plasmaframes.push(Core.atlas.find(this.name+"-plasma-"+i));
+        }
+    },
+    draw(tile){
+  entity = tile.ent();
+  
+  for(var i = 0; i < this.plasmas; i++){
+    r = this.size * Vars.tilesize - 3 + Mathf.absin(Time.time(), 2 + i * 1, 5 - i * 0.5);
+    Draw.color(this.plasma1, this.plasma2, i / plasmas);
+    Draw.alpha((0.3 + Mathf.absin(Time.time(), 2 + i * 2, 0.3 + i * 0.05)) * entity.warmup);
+    Draw.blend(Blending.additive);
+    Draw.rect(this.reg(this.plasmaRegions[i]), tile.drawx(), tile.drawy(), r, r, Time.time() * (12 + i * 6) * entity.warmup);
+    Draw.blend()
+  }
+  Draw.color();
+  Draw.rect(this.region, tile.drawx(), tile.drawy());
+  Draw.color();
+}
+});
