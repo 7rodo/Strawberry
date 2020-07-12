d1ee7f4544833c867441c82c8f7a7449fd8be6a3
7rodo <7rodovje7878@gmail.com>
2020-07-12 09:48:02 +0100

    Update ravager.js

--- scripts/ravager.js
+++ scripts/ravager.js
@@ -1,4 +1,4 @@
-const spawnerBullet = extend(BasicBulletType, {
+const bull = extend(BasicBulletType, {
   draw(b){
   },
 
@@ -8,49 +8,56 @@ const spawnerBullet = extend(BasicBulletType, {
     ulib.spawnUnit(UnitTypes.wraith, b.getTeam(), b.x, b.y)
   }
 });
-spawnerBullet.instantDisappear = false;
-spawnerBullet.lifetime = 20;
-spawnerBullet.speed = 2;
-spawnerBullet.damage = 25;
 
-const unitSpawner = extendContent(Weapon, "strawberry-unit-spawner" {
+bull.damage = 10;
+bull.hitEffect = Fx.none;
+bull.shootEffect = Fx.none;
+bull.despawnEffect = Fx.none;
+bull.shootEffect = summonEffect;
+bull.smokeEffect = Fx.none;
+
+const ravagerWeapon = extendContent(Weapon, "ravager-equip", {
   load(){
-    this.region = Core.atlas.find("strawberry-unit-spawner-weapon")
+    this.region = Core.atlas.find(this.name)
   }
 });
 
-unitSpawner.reload = 60;
-unitSpawner.alternate = true;
-unitSpawner.length = 15;
-unitSpawner.width = 15;
-unitSpawner.shots = 3;
-unitSpawner.bullet = spawnerBullet;
-unitSpawner.recoil = 9;
-unitSpawner.shootSound = Sounds.artillery;
-unitSpawner.minPlayerDist = 20;
+ravagerWeapon.reload = 60;
+ravagerWeapon.alternate = true;
+ravagerWeapon.shots = 3;
+ravagerWeapon.bullet = bull;
+ravagerWeapon.recoil = 9;
+ravagerWeapon.shootSound = Sounds.shotgun;
+ravagerWeapon.minPlayerDist = 20;
 
 const ravager = extendContent(UnitType, "ravager", {
   load(){
     this.weapon.load();
     this.region = Core.atlas.find(this.name);
     this.baseRegion = Core.atlas.find(this.name + "-base");
-    this.legRegion = Core.atlas.find(this.name + "-leg");
+    this.legRegion = Core.atlas.find(this.name + "leg");
   }
 });
 
-ravager.create(prov(() => extend(GroundUnit, {
-  //code
-}));
+/*ravager.create(prov(() => extend(GroundUnit, {
+    update(){
+      this.super$update();
 
+      if(Mathf.chance(Time.delta() * 0.004)){
+        Effects.effect(summonEffect, this);
+        ulib.spawnUnit(UnitTypes.revenant, this.getTeam(), this.x, this.y)
+      }
+    },
 
-ravager.name = "Ravager";
-ravager.description = "j.";
-ravager.health = 25000;
-ravager.flying = false;
-ravager.mass = 100;
-ravager.targetAir = true;
-ravager.rotateWeapon = false;
-ravager.weaponOffsetY = 0;
-ravager.engineOffset = 1;
-ravager.engineSize = 5;
-ravager.weapon = unitSpawner;
+    onDeath(){
+      Effects.effect(deadEffect, this);
+      Effects.shake(2, 2, this);
+
+      Sounds.bang.at(this);
+      this.item.amount = 0;
+      this.drownTime = 0;
+      Events.fire(EventType.UnitDestroyEvent(this));
+    }
+})));*/
+
+ravager.weapon = ravagerWeapon;
