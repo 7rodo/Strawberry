0f8363e0629644002fe3392799f86201901d79c7
7rodo <7rodovje7878@gmail.com>
2020-07-13 12:39:41 +0100

    Update ghost.js

--- scripts/units/ghost.js
+++ scripts/units/ghost.js
@@ -1,5 +1,5 @@
 const ghostTrail = newEffect(25, e => {
     Draw.alpha(e.fin());
-    Draw.rect(Core.atlas.find('glaive-ship'), e.x, e.y, e.fout()*6, e.fout()*6, e.rot);
+    Draw.rect(Core.atlas.find('genesis-ship'), e.x, e.y, e.fout()*6, e.fout()*6, e.rot);
     Draw.alpha();
 });
