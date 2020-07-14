e38f4027b05564ebe1aa732fabd83cd9aa954b12
7rodo <7rodovje7878@gmail.com>
2020-07-14 19:44:48 +0100

    Script for Steriliser

---
+++ scripts/blocks/crafting/steriliser.js
@@ -0,0 +1,22 @@
+const ster = extendContent(GenericCrafter, "steriliser", {
+  load(){
+    this.region = Core.atlas.find(this.name);
+    this.rotateRegion = Core.atlas.find(this.name + "-rotator");
+    this.topRegion = Core.atlas.find(this.name + "-top");
+  },
+  
+  generateIcons(){
+  return [
+    Core.atlas.find(this.name),
+    Core.atlas.find(this.name + "-rotator"),
+    Core.atlas.find(this.name + "-top")
+  ];},
+   
+  draw(tile){
+    entity = tile.ent();
+    
+    Draw.rect(this.region, tile.drawx(), tile.drawy());
+    Draw.rect(this.rotateRegion, tile.drawx(), tile.drawy(), entity.totalProgress * 3.4);
+    Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
+  }
+});
