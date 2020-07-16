bf1246386413644fe48dee9bbaedf34c31322465
7rodo <7rodovje7878@gmail.com>
2020-07-16 10:59:13 +0100

    Update volcano.js

--- scripts/blocks/environment/volcano.js
+++ scripts/blocks/environment/volcano.js
@@ -1,24 +1,23 @@
-const idleeffect = newEffect(200, e => {
+const idleEffect = newEffect(200, e => {
     Draw.color(Color.valueOf("5c5c5c"));
     Draw.alpha(e.fout());
     Fill.circle(e.x, e.y, e.fin()*5);
     Draw.reset();
 });
-const lavaeffect = newEffect(70, e => {
+const lavaEffect = newEffect(70, e => {
     Draw.color(Color.valueOf("e65c17"));
     Draw.alpha(e.fout());
     Lines.circle(e.x, e.y, e.fin()*2);
     Lines.circle(e.x, e.y, e.fin()*3);
     Draw.reset();
 });
-const lavashooteffect = newEffect(12, e => {
+const lavaShootEffect = newEffect(12, e => {
     Draw.color(Color.valueOf("e82700"));
     Lines.circle(e.x, e.y, e.fin());
     Draw.reset();
 });
-const volcanoBullet = extend(ArtilleryBulletType, {
-   // draw(b){}
-});
+const volcanoBullet = extend(ArtilleryBulletType, {});
+
 volcanoBullet.speed = 3;
 volcanoBullet.damage = 0;
 volcanoBullet.lifetime = 180;
@@ -31,16 +30,16 @@ const volcano = extendContent(ArtilleryTurret, "volcano", {
 	update(tile){
 		this.super$update(tile);
 		if(tile.entity.timer.get(this.maintimer, 50)){
-			Effects.effect(idleeffect, Mathf.random(tile.drawx()-5, tile.drawx()+5), Mathf.random(tile.drawy()-5, tile.drawy()+5));
+			Effects.effect(idleEffect, Mathf.random(tile.drawx()-5, tile.drawx()+5), Mathf.random(tile.drawy()-5, tile.drawy()+5));
 		};
 		if(tile.entity.timer.get(this.lavatimer, 30)){
-			Effects.effect(lavaeffect, Mathf.random(tile.drawx()-3, tile.drawx()+3), Mathf.random(tile.drawy()-3, tile.drawy()+3));
+			Effects.effect(lavaEffect, Mathf.random(tile.drawx()-3, tile.drawx()+3), Mathf.random(tile.drawy()-3, tile.drawy()+3));
 		};
 	},
 	updateShooting(tile){
 		this.super$updateShooting(tile);
 		if(tile.entity.timer.get(this.shoottimer, 5)){
-			Effects.effect(lavashooteffect, Mathf.random(tile.drawx()-5, tile.drawx()+5), Mathf.random(tile.drawy()-5, tile.drawy()+5));
+			Effects.effect(lavashootEffect, Mathf.random(tile.drawx()-5, tile.drawx()+5), Mathf.random(tile.drawy()-5, tile.drawy()+5));
 		};
 		this.shoot(tile, volcanoBullet);
 	},
