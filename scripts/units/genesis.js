eeddfc4107c54e9198ec5ec34efd84712f036068
7rodo <7rodovje7878@gmail.com>
2020-07-17 16:07:25 +0100

    Update genesis.js

--- scripts/units/genesis.js
+++ scripts/units/genesis.js
@@ -3,3 +3,15 @@ const ghostTrail = newEffect(25, e => {
     Draw.rect(Core.atlas.find('genesis-ship'), e.x, e.y, e.fout()*6, e.fout()*6, e.rot);
     Draw.alpha();
 });
+
+const genesis = extendContent(UnitType, "strawberry-genesis-ship", {});
+
+genesis.create(prov(() => extend(FlyingUnit, {
+  draw(){
+  this.super$draw();
+  ghostTrail.at(this.x, this.y, this.rotation);
+}
+  if (!Vars.state.isPaused()){
+    Effects.effect(ghostTrail, this.x, this.y, this.rotation);
+}
+})));
