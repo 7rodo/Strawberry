60f7cb0e325bc30102a35f57029612202e184cba
Gdeft <gdeftno@gmail.com>
2020-07-19 22:56:42 +0700

    Fix Genesis

--- scripts/units/genesis.js
+++ scripts/units/genesis.js
@@ -1,17 +1,56 @@
 const ghostTrail = newEffect(25, e => {
-    Draw.alpha(e.fin());
-    Draw.rect(Core.atlas.find('genesis-ship'), e.x, e.y, e.fout()*6, e.fout()*6, e.rot);
-    Draw.alpha();
+    Draw.alpha(Mathf.random(0.7, 1));
+    Fill.circle(e.x, e.y, e.fout() * 3.4)
 });
 
-const genesis = extendContent(UnitType, "strawberry-genesis-ship", {});
+const genesisBullet = extend(BasicBulletType, {});
 
-genesis.create(prov(() => extend(FlyingUnit, {
-  draw(){
-  this.super$draw();
-  ghostTrail.at(this.x, this.y, this.rotation);
-  if (!Vars.state.isPaused()){
-    Effects.effect(ghostTrail, this.x, this.y, this.rotation);
+genesisBullet.bulletWidth = 9
+genesisBullet.bulletHeight = 5
+genesisBullet.lifetime = 60
+genesisBullet.speed = 5
+genesisBullet.damage = 10
+genesisBullet.frontColor = Color.white
+genesisBullet.backColor = Color.valueOf("00dede")
+//genesisBullet.speedMultiplier = 0.25
+//genesisBullet.armorMultiplier = 1.5
+
+const genesisWeapon = extendContent(Weapon, "genesis-equiph", {
+  load(){
+    this.region = Core.atlas.find("clear")
   }
-}
-})));
+});
+
+genesisWeapon.reload = 50;
+genesisWeapon.alternate = true;
+//genesisWeapon.length = 12;
+//genesisWeapon.width = 15;
+genesisWeapon.shots = 2;
+//genesisWeapon.recoil = 9;
+//genesisWeapon.shootSound = Sounds.shotgun;
+//genesisWeapon.minPlayerDist = 20;
+genesisWeapon.bullet = genesisBullet;
+
+const genesis = extendContent(Mech, "genesis-ship", {
+  load(){
+    this.weapon.load();
+    this.region = Core.atlas.find(this.name);
+  },
+
+  updateAlt(player){
+    if (!Vars.state.isPaused()) Effects.effect(ghostTrail, player.x, player.y - 5, player.rotation);
+  }
+});
+
+genesis.weapon = genesisWeapon;
+genesis.localizedName = "Genesis"
+genesis.description = "An ice type mech, penetrates enemies."
+genesis.speed = 0.8
+genesis.flying = true
+genesis.health = 225
+genesis.engineColor = Color.valueOf('00ffff')
+genesis.range = 150
+genesis.engineOffset = 5.3
+genesis.drillPower = 2
+genesis.mineSpeed = 4
+genesis.itemCapacity = 25
