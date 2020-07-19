fec3c90110ed253343f7c1931f765b2940b9f151
7rodo <7rodovje7878@gmail.com>
2020-07-19 09:28:39 +0100

    Update thermal-reactor.js

--- scripts/blocks/power/thermal-reactor.js
+++ scripts/blocks/power/thermal-reactor.js
@@ -16,14 +16,19 @@ const thermalReactor = extendContent(ThermalGenerator, "thermal-reactor", {
     
     Draw.rect(this.region, tile.drawx(), tile.drawy());
     
+    if(entity.warmup > 0 && this.flameColor.a > 0.001){
             const g = 0.4;
             const r = 0.07;
-            const cr = Mathf.random(0.1);            
-            Draw.tint(Color.valueOf("0000ff"));
+            const cr = Mathf.random(0.1);
+            
+            Draw.alpha(((1.0 - g) + Mathf.absin(Time.time(), 9.0, g) + Mathf.random(r) - r) * entity.warmup);
+            
+            Draw.tint(this.flameColor);
             Draw.blend(Blending.additive);
-            Draw.color(Color.valueOf("f2613e"));
+            Draw.color(Color.valueOf("f2613e"), entity.warmup);
             Draw.rect(this.topRegion, tile.drawx(), tile.drawy(), 20.0 + Mathf.absin(Time.time(), 6.0, 5.0), 20.0 + Mathf.absin(Time.time(), 6.0, 5.0));
             Draw.blend();
         }
         Draw.color();
+  }
 });
