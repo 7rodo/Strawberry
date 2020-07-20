ed3d102f841669fa695be616bbe0db73dc90bb5b
7rodo <7rodovje7878@gmail.com>
2020-07-20 09:55:40 +0100

    Update thermal-reactor.js

--- scripts/blocks/power/thermal-reactor.js
+++ scripts/blocks/power/thermal-reactor.js
@@ -4,7 +4,7 @@ const thermalReactor = extendContent(ThermalGenerator, "thermal-reactor", {
    this.topRegion = Core.atlas.find(this.name + "-top");
   },
     
-  generateIcons(){
+    generateIcons(){
     return [
      Core.atlas.find(this.name),
      Core.atlas.find(this.name + "-top"),
@@ -16,17 +16,21 @@ const thermalReactor = extendContent(ThermalGenerator, "thermal-reactor", {
     
     Draw.rect(this.region, tile.drawx(), tile.drawy());
     
+    if(entity.warmup > 0 && this.flameColor.a > 0.001){
             const g = 0.4;
             const r = 0.07;
             const cr = Mathf.random(0.1);
             
-     //       Draw.alpha(((1.0 - g) + Mathf.absin(Time.time(), 9.0, g) + Mathf.random(r) - r));
+            Draw.alpha(((1.0 - g) + Mathf.absin(Time.time(), 9.0, g) + Mathf.random(r) - r) * entity.warmup);
             
-            Draw.tint(Color.valueOf("0000ff"));
+            Draw.tint(this.flameColor);
             Draw.blend(Blending.additive);
-            Draw.color(Color.valueOf("f2613e"));
+            Draw.color(Color.valueOf("f2613e"), entity.warmup);
             Draw.rect(this.topRegion, tile.drawx(), tile.drawy(), 20.0 + Mathf.absin(Time.time(), 6.0, 5.0), 20.0 + Mathf.absin(Time.time(), 6.0, 5.0));
             Draw.blend();
         }
         Draw.color();
+  }
 });
+
+
