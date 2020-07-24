30228d1aa5570c1c7fb1017198253271c4c5a251
7rodo <7rodovje7878@gmail.com>
2020-07-24 13:13:25 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,6 @@ require('ulib');
 
 require('blocks/crafting/purifier');
 require('blocks/crafting/sterilizer');
-//require('blocks/power/thermal-reactor');
 //require('blocks/environment/volcano');
 
 require('units/genesis');
