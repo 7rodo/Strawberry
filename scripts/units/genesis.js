9289309a7fd50f1522c11cc08580023487d39cc5
7rodo <7rodovje7878@gmail.com>
2020-07-24 13:21:57 +0100

    Update genesis.js

--- scripts/units/genesis.js
+++ scripts/units/genesis.js
@@ -1,4 +1,4 @@
-const ghostTrail = newEffect(50, e => {
+const ghostTrail = newEffect(70, e => {
    // var offset = 0.3 + e.fin() * 0.4;
  //   var angle = Mathf.randomSeed(e.id, 360);
     Draw.alpha(Mathf.random(0.3, 0.5));
