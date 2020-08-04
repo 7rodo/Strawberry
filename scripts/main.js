ad3405b78d900105b93c80ab6c8ebe2e74647b40
7rodo <7rodovje7878@gmail.com>
2020-08-04 13:11:08 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ require('ulib');
 
 require('blocks/crafting/purifier');
 require('blocks/crafting/sterilizer');
-//require('blocks/environment/volcano');
+require('blocks/environment/volcano');
 
 require('units/genesis');
 require('units/ravager');
