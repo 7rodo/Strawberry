151084036aec744c8094c67d92296918d3d53145
7rodo <7rodovje7878@gmail.com>
2020-08-05 19:07:14 +0100

    Update overclocker.js

--- scripts/blocks/defense/overclocker.js
+++ scripts/blocks/defense/overclocker.js
@@ -5,7 +5,7 @@ const overclock = extendContent(OverdriveProjector, 'overclocker', {
     this.liquidRegion = Core.atlas.find(this.name + '-liquid');
     this.region = Core.atlas.find(this.name);
     this.topRegion = Core.atlas.find(this.name + '-top');
-  }
+  },
   generateIcons(){
     return [
       Core.atlas.find(this.name + '-bottom'),
@@ -13,7 +13,7 @@ const overclock = extendContent(OverdriveProjector, 'overclocker', {
       Core.atlas.find(this.name),
       Core.atlas.find(this.name + '-top')
     ];
-  }
+  },
   draw(tile){
     entity = tile.ent();
     
@@ -21,6 +21,6 @@ const overclock = extendContent(OverdriveProjector, 'overclocker', {
     Draw.rect(this.liquidRegion, tile.drawx(), tile.drawy());
     Draw.rect(this.region, tile.drawx(), tile.drawy());
     Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
-    }
+    },
   }
 );
