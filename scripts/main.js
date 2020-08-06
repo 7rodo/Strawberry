873e9b0c57ab0fe0c48fce68601e3f627f82f369
7rodo <7rodovje7878@gmail.com>
2020-08-06 19:07:15 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ require('blocks/crafting/purifier');
 require('blocks/crafting/sterilizer');
 require('blocks/environment/volcano');
 require('blocks/defense/overclocker');
+require('blocks/defense/recoverer');
 
 require('units/genesis');
 require('units/ravager');
