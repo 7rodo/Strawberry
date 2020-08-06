c9aee51c121e549b2e199a0fcf84c56a3687de33
7rodo <7rodovje7878@gmail.com>
2020-08-06 11:25:48 +0100

    Update overclocker.js

--- scripts/blocks/defense/overclocker.js
+++ scripts/blocks/defense/overclocker.js
@@ -22,8 +22,15 @@ const overclock = extendContent(OverdriveProjector, 'overclocker', {
     Draw.alpha(entity.liquids.total() / this.liquidCapacity);
     Draw.rect(this.liquidRegion, tile.drawx(), tile.drawy());
     Draw.color();
-    Draw.rect(this.region, tile.drawx(), tile.drawy());
-    Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
-    },
+   // Draw.rect(this.region, tile.drawx(), tile.drawy());
+        Draw.rect(Core.atlas.find(this.name), tile.drawx(), tile.drawy());
+        const f = 1 - (Time.time() / 100) % 1;
+        Draw.color(this.baseColor);    
+        Draw.alpha(entity.power.status * Mathf.absin(Time.time(), 10, 1) * 0.5);
+        Draw.rect(Core.atlas.find(this.name + "-top"), tile.drawx(), tile.drawy());
+        Draw.alpha(1);
+        Lines.stroke((2 * f + 0.2) * entity.power.status);
+        Lines.square(tile.drawx(), tile.drawy(), ((1 - f) * 8) * this.size / 2);
+        Draw.reset();    },
   }
 );
