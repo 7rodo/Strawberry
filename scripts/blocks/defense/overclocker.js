0b973d60012e89a94fc4dacd24c4e99185a3fc98
7rodo <7rodovje7878@gmail.com>
2020-08-07 20:03:53 +0100

    Update overclocker.js

--- scripts/blocks/defense/overclocker.js
+++ scripts/blocks/defense/overclocker.js
@@ -26,7 +26,7 @@ const overclock = extendContent(OverdriveProjector, 'overclocker', {
     if (entity.power.status > 0.01 && entity.liquids.total() > 0.01) {
     Draw.alpha(entity.power.status * Mathf.absin(Time.time(), 10, 1) * 0.5);
     }
-    Draw.alpha(entity.power.status * Mathf.absin(Time.time(), 10, 1) * 0.5);
+    //Draw.alpha(entity.power.status * Mathf.absin(Time.time(), 10, 1) * 0.5);
     Draw.rect(Core.atlas.find(this.name + "-top"), tile.drawx(), tile.drawy());
     Draw.alpha(1);
     Lines.stroke((2 * f + 0.2) * entity.power.status);
