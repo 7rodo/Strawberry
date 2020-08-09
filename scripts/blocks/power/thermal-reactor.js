6d967e8a7611d2208faf7df30bcb663f9b3a428e
7rodo <7rodovje7878@gmail.com>
2020-08-09 21:47:47 +0100

    Create thermal-reactor.js

---
+++ scripts/blocks/power/thermal-reactor.js
@@ -0,0 +1,26 @@
+const thermalReactor = extendContent(ThermalGenerator, "thermal-reactor", {
+ load(){
+   this.region = Core.atlas.find(this.name);
+   this.topRegion = Core.atlas.find(this.name + "-top");
+  },
+    
+  generateIcons(){
+    return [
+     Core.atlas.find(this.name + '-icon'),
+      ]
+    },
+    
+    draw(tile){
+      entity = tile.ent();
+    
+    Draw.rect(this.region, tile.drawx(), tile.drawy());
+    const f = 1 - (Time.time() / 100) % 1;
+    Draw.color(this.baseColor);    
+    if (entity.productionEfficiency > 0.01) {
+    Draw.alpha(entity.productionEfficiency * Mathf.absin(Time.time(), 10, 1) * 0.5);
+    Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
+    Draw.alpha(1);
+    Draw.blend(Blending.additive);
+    }
+    Draw.reset();   
+    });
