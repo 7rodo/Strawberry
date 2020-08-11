71e348f203686ba201a8b47adb0cedd650264342
7rodo <7rodovje7878@gmail.com>
2020-08-11 14:21:13 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,24 @@
+// What ever the j this is
 require('ulib');
 
+// Crafting
 require('blocks/crafting/purifier');
 require('blocks/crafting/sterilizer');
-require('blocks/environment/volcano');
+
+// Defensive
 require('blocks/defense/overclocker');
 require('blocks/defense/recoverer');
+
+// Turrets
+require('blocks/turrets/buckshot');
+
+// Environment
+require('blocks/environment/volcano');
+
+// Power
 require('blocks/power/thermal-reactor');
 
+// Units
 require('units/genesis');
 require('units/ravager');
 require('units/bull');
