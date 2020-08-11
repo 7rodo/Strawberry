8c9a16550905d3444a63203f25f47151fbb53d36
7rodo <7rodovje7878@gmail.com>
2020-08-11 14:58:59 +0100

    Update buckshot.js

--- scripts/blocks/turrets/buckshot.js
+++ scripts/blocks/turrets/buckshot.js
@@ -1,6 +1,6 @@
 const buckshotStandard = extend(BasicBulletType, {
 });
-buckshotStandard.speed = 5;
+buckshotStandard.speed = 3 + Mathf.random(1,5);
 buckshotStandard.damage = 10;
 buckshotStandard.lifetime = 70;
 buckshotStandard.bulletHeight = 5 + Mathf.random(3,9);
@@ -8,7 +8,7 @@ buckshotStandard.bulletWidth = buckshotBullet.bulletHeight * 2;
 
 const buckshotDense = extend(BasicBulletType, {
 });
-buckshotDense.speed = 4;
+buckshotDense.speed = 2 + Mathf.random(2,3);
 buckshotDense.damage = 29;
 buckshotDense.lifetime = 90;
 buckshotDense.lifetime = 90;
@@ -19,7 +19,7 @@ buckshotDense.backColor = Color.valueOf('64c1e3');
 
 const buckshotIncendiary = extend(BasicBulletType, {
 });
-buckshotIncendiary.speed = 5;
+buckshotIncendiary.speed = 4 + Mathf.random(1,6);
 buckshotIncendiary.damage = 11;
 buckshotIncendiary.lifetime = 60;
 buckshotIncendiary.incendAmount = 1;
@@ -32,7 +32,7 @@ buckshotIncendiary.backColor = Color.valueOf('ff8400');
 
 const buckshotHoming = extend(BasicBulletType, {
 });
-buckshotHoming.speed = 6;
+buckshotHoming.speed = 2 + Mathf.random(1,5);
 buckshotHoming.damage = 9;
 buckshotHoming.lifetime = 90;
 buckshotHoming.homingPower = 0.02;
@@ -42,7 +42,7 @@ buckshotHoming.bulletWidth = buckshotBullet.bulletHeight * 2;
 
 const buckshotExplosive = extend(BasicBulletType, {
 });
-buckshotExplosive.speed = 5;
+buckshotExplosive.speed = 1 + Mathf.random(2,5);
 buckshotExplosive.damage = 16;
 buckshotExplosive.lifetime = 60;
 buckshotExplosive.homingPower = 0.02;
