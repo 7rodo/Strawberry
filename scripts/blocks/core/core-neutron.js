7772589ab76f2d9e067965cb7519dc9823fe9606
7rodo <7rodovje7878@gmail.com>
2020-09-14 17:27:17 +0100

    Update core-neutron.js

--- scripts/blocks/core/core-neutron.js
+++ scripts/blocks/core/core-neutron.js
@@ -1,1 +1,15 @@
-j
+ var placingabi = true;
+ const neutron = extendContent(CoreBlock, "core-neutron", {
+     update(tile){
+        entity = tile.ent();
+        placingabi = false;
+      this.super$update(tile);
+    },
+  onDestroyed(tile){},
+   removed(tile){
+      placingabi = true;
+     },
+ canPlaceOn(tile){
+        return placingabi;
+     }
+});
