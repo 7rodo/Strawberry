b810d3e2d3a55af0dfb7378dfa6b631b43d56ce6
7rodo <7rodovje7878@gmail.com>
2020-10-19 12:32:13 +0100

    Update fusion-churner.js

--- scripts/blocks/crafting/fusion-churner.js
+++ scripts/blocks/crafting/fusion-churner.js
@@ -32,13 +32,9 @@ const churner = extendContent(GenericSmelter, "fusion-churner", {
   }
 });
 
-const sss = newEffect(30, e => {
-  Draw.color(Color.valueOf("ffb382"), Color.valueOf("ff8282"), e.fin());
-  const h = new Floatc2({get: function(x, y){
-    Draw.color(Color.valueOf("ffb382"), Color.valueOf("ff8282"), e.fin());
-    Fill.circle(e.x + x, e.y + y, e.fout() * 3);
-  }});
-  Angles.randLenVectors(e.id, 17, 7 + e.fin() * 3, e.rotation,  (e.x, e.y), h);
-});
+const j = newEffect(20, e => {
+  Draw.color(Pal.lancerLaser);
+  Lines.square(e.x, e.y, 30 * e.fin(), 45);
+})
 
-churner.updateEffect = sss;
+churner.updateEffect = j;
