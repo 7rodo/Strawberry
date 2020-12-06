996c6296b6ec11e5da33fcfefda0d1be9b82df16
7rodo <7rodovje7878@gmail.com>
2020-12-06 18:16:37 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 // What ever the j this is
-require('strawberry/ulib');
+//require('strawberry/ulib');
 
 // Crafting
 require('strawberry/blocks/crafting/purifier');
@@ -8,18 +8,17 @@ require('strawberry/blocks/crafting/fusion-churner');
 
 // Defensive
 require('strawberry/blocks/defense/overclocker');
-require('strawberry/blocks/defense/recoverer');
+//require('strawberry/blocks/defense/recoverer');
 
 // Turrets
-require('strawberry/blocks/turrets/buckshot');
+//require('strawberry/blocks/turrets/buckshot');
 
 // Environment
-require('strawberry/blocks/environment/volcano');
+//require('strawberry/blocks/environment/volcano');
 
 // Power
 require('strawberry/blocks/power/thermal-reactor');
 
 // Units
-require('strawberry/units/genesis');
-require('strawberry/units/ravager');
-require('strawberry/units/bull');
+//require('strawberry/units/genesis');
+//require('strawberry/units/ravager');
