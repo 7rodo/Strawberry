dd05e2dd8f0a2c21621a50cd088ee37c659bc080
7rodo <7rodovje7878@gmail.com>
2020-12-11 14:16:59 +0000

    Update recoverer.js

--- scripts/blocks/defense/recoverer.js
+++ scripts/blocks/defense/recoverer.js
@@ -26,7 +26,7 @@ recover.buildType = () => extend(MendProjector.MendBuild, recover, {
     Draw.rect(reco.liquidRegion, this.x, this.y);
     Draw.color();
 
-    Draw.rect(this.region, this.x, this.y);
+    Draw.rect(reco.region, this.x, this.y);
 
     let f = 1 - (Time.time / 100) % 1;
     Draw.color(reco.baseColor);    
