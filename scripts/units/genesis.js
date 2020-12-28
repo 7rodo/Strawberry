799d3abb8f72d31ca4d856201dd92a16db051bff
7rodo <7rodovje7878@gmail.com>
2020-12-28 08:29:18 +0000

    Update genesis.js

--- scripts/units/genesis.js
+++ scripts/units/genesis.js
@@ -1,24 +1,20 @@
 const ghostTrail = newEffect(70, e => {
-   // var offset = 0.3 + e.fin() * 0.4;
- //   var angle = Mathf.randomSeed(e.id, 360);
     Draw.alpha(Mathf.random(0.3, 0.5));
     Draw.color(Pal.lancerLaser);
     Lines.circle(e.x, e.y, e.fout() * 3.4);
-  //  Lines.circle(e.x + Angles.trnsx(angle, offset), e.y + Angles.trnsy(angle, offset), e.rotation - 90);
 });
 
 const genesisBullet = extend(BasicBulletType, {});
 
-genesisBullet.bulletWidth = 10
-genesisBullet.bulletHeight = 5
+genesisBullet.width = 10
+genesisBullet.length = 5
 genesisBullet.lifetime = 60
-genesisBullet.speed = 7
-genesisBullet.damage = 5
+genesisBullet.speed = 5
+genesisBullet.damage = 2
 genesisBullet.frontColor = Color.white
 genesisBullet.backColor = Color.valueOf("00dede")
 genesisBullet.pierce = true;
 genesisBullet.status = StatusEffects.freezing;
-genesisBullet.speedMultiplier = 0.667
 
 const genesisWeapon = extendContent(Weapon, "genesis-equiph", {
   load(){
@@ -32,7 +28,7 @@ genesisWeapon.shots = 3;
 genesisWeapon.shootSound = Sounds.pew;
 genesisWeapon.bullet = genesisBullet;
 
-const genesis = extendContent(flying, "genesis-ship", {
+const genesis = extendContent(air, "genesis-ship", {
   load(){
     this.weapon.load();
     this.region = Core.atlas.find(this.name);
@@ -45,13 +41,12 @@ const genesis = extendContent(flying, "genesis-ship", {
 
 genesis.weapon = genesisWeapon;
 genesis.localizedName = "Genesis"
-genesis.description = "An ice type mech, penetrates enemies."
+genesis.description = "freeeeeeeeeeeeeeeeeeze"
 genesis.speed = 0.8
 genesis.flying = true
 genesis.health = 225
-genesis.engineColor = Color.valueOf('00ffff')
-genesis.range = 150
+genesis.range = 100
 genesis.engineOffset = 5.3
-genesis.drillPower = 2
+genesis.mineTier = 2
 genesis.mineSpeed = 4
 genesis.itemCapacity = 25
