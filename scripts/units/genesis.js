9d2c71431ef56f2301443e219037d44cd88bcead
7rodo <7rodovje7878@gmail.com>
2020-12-29 11:41:36 +0000

    Update genesis.js

--- scripts/units/genesis.js
+++ scripts/units/genesis.js
@@ -1,3 +1,3 @@
 const GenesisT1 = extendContent(UnitType, "genesis-ship", {});
 GenesisT1.constructor = () => extend(UnitEntity, {});
-Blocks.airFactory.plans.add(new UnitFactory.UnitPlan(GenesisT1, 60 * 35, ItemStack.with(Items.silicon, 40, Items.metaglass, 35, Items.titanium, 20)));
+Blocks.airFactory.plans.add(new UnitFactory.UnitPlan(GenesisT1, 60 * 35, ItemStack.with(Items.silicon, 40, Items.metaglass, 35, Items.thorium, 20)));
