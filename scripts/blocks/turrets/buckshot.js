ace808d61e9fe6b07d180112da98063ca79d28ca
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-01-15 17:30:00 +0100

    Update buckshot.js

--- scripts/blocks/turrets/buckshot.js
+++ scripts/blocks/turrets/buckshot.js
@@ -2,7 +2,7 @@ const buckshotStandard = extend(BasicBulletType, {
 });
 buckshotStandard.speed = 5;
 buckshotStandard.damage = 10;
-buckshotStandard.lifetime = 70;
+buckshotStandard.lifetime = 45;
 buckshotStandard.bulletHeight = 5;
 buckshotStandard.bulletWidth = 3;
 
@@ -10,7 +10,7 @@ const buckshotDense = extend(BasicBulletType, {
 });
 buckshotDense.speed = 5;
 buckshotDense.damage = 20;
-buckshotDense.lifetime = 90;
+buckshotDense.lifetime = 45;
 buckshotDense.reloadMultiplier = 0.6;
 buckshotDense.bulletHeight = 7.5;
 buckshotDense.bulletWidth = 4.5;
@@ -20,7 +20,7 @@ const buckshotIncendiary = extend(BasicBulletType, {
 });
 buckshotIncendiary.speed = 6;
 buckshotIncendiary.damage = 11;
-buckshotIncendiary.lifetime = 60;
+buckshotIncendiary.lifetime = 40;
 buckshotIncendiary.incendAmount = 1;
 buckshotIncendiary.incendSpread = 4;
 buckshotIncendiary.ammoMultiplier = 3;
@@ -32,12 +32,13 @@ buckshotIncendiary.backColor = Color.valueOf('ff8400');
 const buckshotHoming = extend(BasicBulletType, {
 });
 buckshotHoming.speed = 5;
-buckshotHoming.damage = 9;
-buckshotHoming.lifetime = 90;
-buckshotHoming.homingPower = 0.02;
+buckshotHoming.damage = 13;
+buckshotHoming.lifetime = 45;
+buckshotHoming.homingPower = 0.1;
 buckshotHoming.ammoMultiplier = 2;
 buckshotHoming.bulletHeight = 5;
 buckshotHoming.bulletWidth = 3;
+buckshotHoming.reloadMultiplier = 1.5;
 
 const buckshotExplosive = extend(BasicBulletType, {
 });
